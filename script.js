71d0a6b8dc0bcef533e41006bdaf73cf1f87c757
Anil Bhatt <82743529+AnilBhatt01@users.noreply.github.com>
2023-02-08 11:48:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,22 @@ button.addEventListener("click", calculateLove);
 function calculateLove() {
   let yourName = yourInput.value;
   let yourCrush = crushInput.value;
+  let yourNameUp = yourName.toUpperCase();
+  let yourCrushUp = yourCrush.toUpperCase();
+
+  console.log(yourNameUp, yourCrushUp);
 
   let percent = Math.ceil(Math.random() * 100);
-
-  if (yourName === "anil") {
+  if (yourName === "" || yourCrush === "") {
+    result.textContent = "Enter name";
+  } else if (yourNameUp === "ANIL" && yourCrushUp === "SAMHITA") {
     result.textContent =
       "You both are made for each other, your love can't be calculated in any calculator";
-  } else if (yourName !== "anil" && yourCrush === "coding") {
+  } else if (yourNameUp !== "ANIL" && yourCrushUp === "SAMHITA") {
     result.textContent =
       "This person will surely make you cry. Look for anyone else";
   } else {
-    result.textContent = `${yourName} and ${yourCrush} have ${percent}% love`;
+    result.textContent = `${yourNameUp} and ${yourCrushUp} have ${percent}% love`;
   }
   result.style.display = "block";
 }
